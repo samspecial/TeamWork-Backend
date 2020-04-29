4d9e2e71b5b11dd164cc211badf61e739c2cf0b3
Osinloye Samuel Adebayo <psalmueloye@gmail.com>
2020-04-29 23:49:20 +0100

    Added cors module

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 
 
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const adminRoutes = require('../controllers/adminController');
 
 
-router.post('/create-user', [body('email').isEmail().withMessage('Email not valid'), body('password', 'Password must be alphanumeric').isAlphanumeric().trim(), body('firstName', 'Field can\'t be empty').isLength({ min: 2 }), body('lastName', 'Field can\'t be empty').isLength({ min: 2 }), body('address').isLength({ min: 2 }), body('department').isLength({ min: 2 }),], adminRoutes.createNewUser);
+router.post('/auth/create-user', [body('email').isEmail().withMessage('Email not valid'), body('password', 'Password must be alphanumeric').isAlphanumeric().trim(), body('firstName', 'Field can\'t be empty').isLength({ min: 2 }), body('lastName', 'Field can\'t be empty').isLength({ min: 2 }), body('address').isLength({ min: 2 }), body('department').isLength({ min: 2 }),], adminRoutes.createNewUser);
 
-router.post('/signin', [body('email').isEmail().withMessage('Email not valid'), body('password', 'Password must be alphanumeric').isAlphanumeric().trim()], adminRoutes.createLogin)
+router.post('/auth/signin', [body('email').isEmail().withMessage('Email not valid'), body('password', 'Password must be alphanumeric').isAlphanumeric().trim()], adminRoutes.createLogin)
 module.exports = router;
