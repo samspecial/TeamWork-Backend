096d1f9946396af7bf18ff10a719f411eadd8997
Osinloye Samuel Adebayo <psalmueloye@gmail.com>
2020-04-30 03:41:34 +0100

    Added tables to database

--- models/comments.sql
+++ models/comments.sql
@@ -1,12 +1,13 @@
-CREATE TABLE comments(
-        commentid INTEGER NOT NULL,
-        comment TEXT,
-        gifid INTEGER,
-        articleid INTEGER,
-        authorid INTEGER,
-        createdon DATE,
-        
-        PRIMARY KEY (commentid),
-        FOREIGN KEY articleid REFERENCES comments(articleid),
-        FOREIGN KEY gifid REFERENCES comments(gifid)
-    );
+CREATE TABLE comments
+(
+    commentid INTEGER NOT NULL,
+    comment TEXT,
+    gifid INTEGER,
+    articleid INTEGER,
+    authorid INTEGER,
+    createdon DATE,
+
+    PRIMARY KEY (commentid),
+    FOREIGN KEY (articleid) REFERENCES articles(articleid),
+    FOREIGN KEY (gifid) REFERENCES gifs(gifid)
+);
--- models/gifs.sql
+++ models/gifs.sql
@@ -1,9 +1,12 @@
- CREATE TABLE gifs(
-        imageurl VARCHAR(100),
-        gifid INTEGER NOT NULL,
-        title VARCHAR(50),
-        createdon DATE,
-        publicid VARCHAR(100),
-        authorid INT,
-        PRIMARY KEY (gifid)
-    );
+
+CREATE TABLE gifs
+(
+    imageurl VARCHAR(100),
+    gifid INTEGER NOT NULL,
+    title VARCHAR(50),
+    createdon DATE,
+    publicid VARCHAR(100),
+    authorid INT,
+
+    PRIMARY KEY (gifid)
+);
