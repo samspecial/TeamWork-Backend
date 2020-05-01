7a70edc293e328c095abb3115f7f64fc735f7c4e
Osinloye Samuel Adebayo <psalmueloye@gmail.com>
2020-05-01 04:38:25 +0100

    fix ssl certificate

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'production') {
     pool = new Pool({
         connectionString: process.env.DATABASE_URL,
         ssl: {
-            rejectUnauthorized: false,
+            rejectUnauthorized: true,
         },
     });
 } else if (process.env.NODE_ENV === 'test') {
